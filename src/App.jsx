5bf00644316fd8973df4169091c2f35e0cc44c42
Sadegh Saedi Nia <s.saedinia@gmail.com>
2024-01-05 00:27:11 +0100

    [FM-5] initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main>
+      <h1>
+        Javascript <i class="fa-brands fa-js"></i>
+      </h1>
+
+      <h1>
+        React <i class="fa-brands fa-react"></i>
+      </h1>
+    </main>
+  );
+}
+
+export default App;
